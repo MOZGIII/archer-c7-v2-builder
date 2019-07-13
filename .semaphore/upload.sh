9b6852a2e2ff72dd85e92a5c7f4233bac8a4c684
MOZGIII <mike-n@narod.ru>
2019-07-14 00:33:06 +0300

    Change artifacts ownership before creating the repo

--- .semaphore/upload.sh
+++ .semaphore/upload.sh
@@ -11,7 +11,10 @@ BRANCH_NAME="${BRANCH_PREFIX}$(date --iso=minute --utc)"
 # Expand private key path before changing directory.
 GIT_PRIVATE_KEY_PATH="$(realpath "$GIT_PRIVATE_KEY_PATH")"
 
+OWNER="$(whoami)"
+
 cd "$CONTENT_PATH"
+sudo chown -R "$OWNER:$OWNER" .
 git init .
 git remote add repo "$GIT_REPO_URL"
 git checkout -b "$BRANCH_NAME"
