c990555ad1586188e428835a23fd7a9f098f8d5b
MOZGIII <mike-n@narod.ru>
2019-07-14 16:52:15 +0300

    Build with libustream-mbedtls package

--- scripts/mkconfig.sh
+++ scripts/mkconfig.sh
@@ -6,6 +6,7 @@ CONFIG_TARGET_ath79=y
 CONFIG_TARGET_ath79_generic=y
 CONFIG_TARGET_ath79_generic_DEVICE_tplink_archer-c7-v2=y
 CONFIG_DEVEL=y
+CONFIG_PACKAGE_libustream-mbedtls=y
 CFG
 
 if [[ "${BUILD_CFG_BUILD_ALL_PACKAGES:-}" == "true" ]]; then
