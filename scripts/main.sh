49a70cda84add8620ca2154b2d32d07e71d20486
MOZGIII <mike-n@narod.ru>
2019-07-14 21:25:12 +0300

    Restructured the main script

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,58 +1,64 @@
 #!/bin/bash
 set -xeuo pipefail
-NAME="$1"; shift
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
 BUILDER_PATH="$(pwd)"
 BUILD_PATH="/build"
 SOURCE_PATH="$BUILD_PATH/source"
 DOWNLOAD_FOLDER="$BUILD_PATH/dl"
 BINARY_FOLDER="$BUILD_PATH/bin"
 
-pushd "$BUILD_PATH"
+# Clone source from our submodules if it's not ready.
+if [[ ! -d "$SOURCE_PATH" ]]; then
+  cd "$BUILD_PATH"
+  clone-source "file://$BUILDER_PATH/submodules/source" --depth 1
+fi
 
-[[ ! -d "$SOURCE_PATH" ]] && clone-source "file://$BUILDER_PATH/submodules/source" --depth 1
+# Go over to the source dir.
+cd "$SOURCE_PATH"
 
-pushd "$SOURCE_PATH"
-
-sed "s|{root}|file://${BUILDER_PATH}/submodules|" "$BUILDER_PATH/files/feeds.conf.template" > "$SOURCE_PATH/feeds.conf"
-
-# Downgrade kernel to 4.14 from 4.19.
+# Once sources are ready, downgrade kernel to 4.14 from 4.19.
 sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.14/' ./target/linux/ath79/Makefile
 
+# Configure feeds to use our submodules.
+sed "s|{root}|file://${BUILDER_PATH}/submodules|" "$BUILDER_PATH/files/feeds.conf.template" > "$SOURCE_PATH/feeds.conf"
+
+# Update and install feeds.
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+# Customize build with our configuration.
 (
   export DOWNLOAD_FOLDER BINARY_FOLDER
   "$BUILDER_PATH/scripts/mkconfig.sh" > .config
 )
 
+# Expand configuration.
 make defconfig
+
+# Download package sources and other dependencies.
 make download "-j$(nproc)"
 
+# Prepare a non-root user to run as.
 useradd --home-dir "$BUILD_PATH" --shell /bin/bash nonroot
 
+# Prepare build path for running as non-root.
 chown -R nonroot:nonroot "$BUILD_PATH"
 
-make_args() {
-  if [[ -n "${BUILD_CFG_IGNORE_ERRORS:-}" ]]; then
-    COMMAND+=( "IGNORE_ERRORS=$BUILD_CFG_IGNORE_ERRORS" )
-  fi
-}
+# Prepare make invocation.
+MAKE_INVOCATION=( "make" )
 
-case "$NAME" in
-  "build")
-    COMMAND=( "make" "-j$(nproc)" )
-    make_args
-    ;;
-  "debug-build")
-    COMMAND=( "make" "-j1" "V=s" )
-    make_args
-    ;;
-  *)
-    echo >&2 "Error: unknown command"
-    exit 2
-    ;;
-esac
+# When debugging build process use special options for better readability.
+if [[ "${BUILD_CFG_BUILD_DEBUGGING:-}" == "true" ]]; then
+  MAKE_INVOCATION+=( "-j1" "V=s" )
+else
+  MAKE_INVOCATION+=( "-j$(nproc)" )
+fi
 
-su-exec nonroot "${COMMAND[@]}"
+# Ignore errors if we're asked to.
+if [[ -n "${BUILD_CFG_IGNORE_ERRORS:-}" ]]; then
+  MAKE_INVOCATION+=( "IGNORE_ERRORS=$BUILD_CFG_IGNORE_ERRORS" )
+fi
+
+# Invoke make.
+su-exec nonroot "${MAKE_INVOCATION[@]}"
