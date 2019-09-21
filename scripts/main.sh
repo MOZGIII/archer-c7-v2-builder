0758223380834836d51e1594cdce441fe27083b5
MOZGIII <mike-n@narod.ru>
2019-09-21 23:29:40 +0300

    Switched to build state machine

--- scripts/main.sh
+++ scripts/main.sh
@@ -7,71 +7,140 @@ BUILD_PATH="$BUILD_CFG_BUILD_PATH"
 SOURCE_PATH="$BUILD_PATH/source"
 DOWNLOAD_FOLDER="$BUILD_PATH/dl"
 BINARY_FOLDER="$BUILD_PATH/bin"
+STEP_FILE_PATH="$BUILD_PATH/step"
 
 check_exit_point() {
-  CURRENT_POINT="$1" ; shift
+  local CURRENT_POINT="$1" ; shift
   if [[ "${BUILD_CFG_EXIT_AFTER:-}" == "$CURRENT_POINT" ]]; then
     echo "Reached exit point $CURRENT_POINT and exiting as requested..." >&2
     exit 0
   fi
 }
 
-# Clone source from our submodules if it's not ready.
-if [[ ! -d "$SOURCE_PATH" ]]; then
-  git clone "file://$BUILDER_PATH/submodules/source" "$SOURCE_PATH" --depth 1
-fi
+read_step() {
+  if [[ -f "$STEP_FILE_PATH" ]]; then
+    cat "$STEP_FILE_PATH"
+  else
+    echo "clone"
+  fi
+}
 
-# Go over to the source dir.
-cd "$SOURCE_PATH"
+write_step() {
+  local STEP="$1"
+  echo "$STEP" > "$STEP_FILE_PATH"
+}
 
-# Once sources are ready, downgrade kernel to 4.14 from 4.19.
-sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.14/' ./target/linux/ath79/Makefile
+before_step() {
+  local STEP="$1"
 
-# Configure feeds to use our submodules.
-sed "s|{root}|file://${BUILDER_PATH}/submodules|" "$BUILDER_PATH/files/feeds.conf.template" > ./feeds.conf
+  if [[ "$STEP" == "clone" ]]; then
+    # Skip change dir at the clone step.
+    return
+  fi
 
-# Update and install feeds.
-./scripts/feeds update -a
-./scripts/feeds install -a
+  # Go over to the source dir.
+  cd "$SOURCE_PATH"
+}
 
-# Customize build with our configuration.
-(
-  export DOWNLOAD_FOLDER BINARY_FOLDER
-  "$BUILDER_PATH/scripts/mkconfig.sh" > ./.config
-)
+execute_step() {
+  local STEP="$1"
+  case "$STEP" in
+    "clone")
+      # Clone source from our submodules if it's not ready.
+      if [[ ! -d "$SOURCE_PATH" ]]; then
+        git clone "file://$BUILDER_PATH/submodules/source" "$SOURCE_PATH" --depth 1
+      fi
 
-# Expand configuration.
-make defconfig
+      # Schedule next execution step.
+      write_step "config"
+      ;;
+    "config")
+      # Once sources are ready, downgrade kernel to 4.14 from 4.19.
+      sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.14/' ./target/linux/ath79/Makefile
 
-# Exit here if requested - can be used to inflate the download cache.
-check_exit_point "config"
+      # Configure feeds to use our submodules.
+      sed "s|{root}|file://${BUILDER_PATH}/submodules|" "$BUILDER_PATH/files/feeds.conf.template" > ./feeds.conf
 
-# Download package sources and other dependencies.
-make download "-j$(nproc)"
+      # Update and install feeds.
+      ./scripts/feeds update -a
+      ./scripts/feeds install -a
 
-# Exit here if requested - can be used to capture the download cache.
-check_exit_point "download"
+      # Customize build with our configuration.
+      (
+        export DOWNLOAD_FOLDER BINARY_FOLDER
+        "$BUILDER_PATH/scripts/mkconfig.sh" > ./.config
+      )
 
-# Prepare a non-root user to run as.
-useradd --home-dir "$BUILD_PATH" --shell /bin/bash nonroot
+      # Expand configuration.
+      make defconfig
 
-# Prepare build path for running as non-root.
-chown -R nonroot:nonroot "$BUILD_PATH"
+      # Schedule next execution step.
+      write_step "download"
+      ;;
+    "download")
+      # Download package sources and other dependencies.
+      make download "-j$(nproc)"
 
-# Prepare make invocation.
-MAKE_INVOCATION=( "make" )
+      # Schedule next execution step.
+      write_step "prebuild"
+      ;;
+    "prebuild")
+      # Prepare a non-root user to run as.
+      useradd --home-dir "$BUILD_PATH" --shell /bin/bash nonroot
 
-# When debugging build process use special options for better readability.
-if [[ "${BUILD_CFG_BUILD_DEBUGGING:-}" == "true" ]]; then
-  MAKE_INVOCATION+=( "-j1" "V=s" )
-else
-  MAKE_INVOCATION+=( "-j$(nproc)" )
-fi
+      # Prepare build path for running as non-root.
+      chown -R nonroot:nonroot "$BUILD_PATH"
 
-# Ignore errors if we're asked to.
-if [[ -n "${BUILD_CFG_IGNORE_ERRORS:-}" ]]; then
-  MAKE_INVOCATION+=( "IGNORE_ERRORS=$BUILD_CFG_IGNORE_ERRORS" )
-fi
+      # Schedule next execution step.
+      write_step "build"
+      ;;
+    "build")
+      # Prepare make invocation.
+      MAKE_INVOCATION=( "make" )
 
-# Invoke make.
-su-exec nonroot "${MAKE_INVOCATION[@]}"
+      # When debugging build process use special options for better readability.
+      if [[ "${BUILD_CFG_BUILD_DEBUGGING:-}" == "true" ]]; then
+        MAKE_INVOCATION+=( "-j1" "V=s" )
+      else
+        MAKE_INVOCATION+=( "-j$(nproc)" )
+      fi
+
+      # Ignore errors if we're asked to.
+      if [[ -n "${BUILD_CFG_IGNORE_ERRORS:-}" ]]; then
+        MAKE_INVOCATION+=( "IGNORE_ERRORS=$BUILD_CFG_IGNORE_ERRORS" )
+      fi
+
+      # Invoke make.
+      su-exec nonroot "${MAKE_INVOCATION[@]}"
+
+      # Schedule next execution step.
+      write_step "done"
+      ;;
+    "done")
+      echo Done
+      exit 0
+      ;;
+  esac
+}
+
+main_loop() {
+  while true; do
+    local STEP
+    # Read the pending step.
+    STEP="$(read_step)"
+
+    # Log the step.
+    echo >&2 "==> $STEP"
+
+    # Invoke common commands before each step.
+    before_step "$STEP"
+
+    # Execute the step.
+    execute_step "$STEP"
+
+    # Exit here if requested.
+    check_exit_point "$STEP"
+  done
+}
+
+main_loop
